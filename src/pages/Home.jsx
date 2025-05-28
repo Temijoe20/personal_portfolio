1f261fdfac06e906d0584e77c3b6ff5e9d956adf
unknown <abijo356@gmail.com>
2025-05-28 12:31:37 +0100

    second commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@
 import { ThemeToggle } from "../components/ThemeToggle";
 import { StarBackground } from "@/components/StarBackground";
 import { HeroSection } from "../components/HeroSection";
+import { AboutMe } from "../components/AboutMe";
 export const Home = () => {
     return( 
         <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
@@ -14,6 +15,7 @@ export const Home = () => {
            {/* Main Content */}
            <main>
                 <HeroSection />
+                <AboutMe />
            </main>
 
             {/* Footer */}
